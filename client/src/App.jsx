e082810c1fd252a8fdc71240f11e343e255befe4
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-08-29 18:47:46 +0300

    Added routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
+import Home from './components/home/Home';
+import AboutUs from './components/aboutUs/AboutUs';
 
 function App() {
 
   return (
     <>
-    <Header />
-    <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
