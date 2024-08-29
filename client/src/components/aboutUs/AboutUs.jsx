5743ddffc35caa647771d989ca7d3d733dc98e1a
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-08-29 18:45:58 +0300

    Created aboutUs component with css module for it

---
+++ client/src/components/aboutUs/AboutUs.jsx
@@ -0,0 +1,135 @@
+import styles from './AboutUs.module.css'
+
+export default function AboutUs() {
+    return (
+        <>
+            <div className={styles.forms}>
+                <div className={styles.form5}>
+                    <img src="./images/form5.png" alt="form5" />
+                </div>
+                <div className={styles.form6}>
+                    <img src="./images/form6.png" alt="form6" />
+                </div>
+                <div className={styles.form7}>
+                    <img src="./images/form7.png" alt="form7" />
+                </div>
+                <div className={styles.form8}>
+                    <img src="./images/form8.png" alt="form8" />
+                </div>
+                <div className={styles.form9}>
+                    <img src="./images/form9.png" alt="form9" />
+                </div>
+                <div className={styles.form10}>
+                    <img src="./images/form10.png" alt="form10" />
+                </div>
+            </div>
+            <div className={styles.whoAreThey}>
+                <h1>
+                    Кои са <br /> July morning?
+                </h1>
+            </div>
+            <div className={styles.team}>
+                <div className={styles.member1}>
+                    <div className={styles.description1}>
+                        <h2 className={styles.name}>Кристи-Анна Шаламанова</h2>
+                        <h3 className={styles.position}>Progect manager / Реклама и маркетинг</h3>
+                        <p className={styles.text}>
+                            Здравейте, казвам се Кристи-Анна Шаламанова <br /> и уча в Национална{" "}
+                            <br /> Търговска Гимназия - Пловдив. <br /> Занимавам се с маркетинг и
+                            SEO като <br /> имам интерес и в сферите на бизнес и <br />
+                            project management.
+                        </p>
+                    </div>
+                    <div className={styles["person-image"]}>
+                        <a href="https://www.instagram.com/s1mply.kr1s1/" target="_blank">
+                            <img src="./images/kristi-anna.png" alt="kristi-anna" />
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.member2}>
+                    <div className={styles["person-image"]}>
+                        <a href="https://www.instagram.com/vilianakovacheva/" target="_blank">
+                            <img src="./images/viliana.png" alt="viliana" />
+                        </a>
+                    </div>
+                    <div className={styles.description2}>
+                        <h2 className={styles.name}>Вилиана Ковачева</h2>
+                        <h3 className={styles.position}>Front-End Development</h3>
+                        <p className={styles.text}>
+                            Амбициозна, старателна и <br /> целеустремена личност съм с ясна{" "}
+                            <br /> насоченост към професионалното си <br /> развитие в областта на
+                            софтуерното <br /> инженерство. Обучавам се в <br />
+                            програмата за софтуерно инженерство <br /> с JavaScript в SoftUni,
+                            където успешно <br /> съм завършила курсовете <br /> "Programming
+                            Basics with JavaScript", <br /> "Programming Fundamentals with <br />
+                            JavaScript", "JS Advanced", "JS <br /> Applications", "TypeScript",
+                            "JS Back-End" <br /> и "ReactJS".
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.member1}>
+                    <div className={styles.description1}>
+                        <h2 className={styles.name}>Стратимира Боянова</h2>
+                        <h3 className={styles.position}>(Web) Дизайн</h3>
+                        <p className={styles.text}>
+                            Здравейте, казвам се Стратимира. <br /> Занимавам се с графичен дизайн
+                            и <br /> фотография. Имам интерес да се <br /> занимавам в сферите на
+                            графичен <br /> дизайн и архитектура. Имам взети <br />
+                            успешно курсове и спечелила награди <br /> в тези области.
+                        </p>
+                    </div>
+                    <div className={styles["person-image"]}>
+                        <a href="https://www.instagram.com/stratimira/" target="_blank">
+                            <img src="./images/stratimira.png" alt="stratimira" />
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.member2}>
+                    <div className={styles["person-image"]}>
+                        <a href="https://www.instagram.com/victorthebigfrog/" target="_blank">
+                            <img src="./images/viktor.png" alt="viktor" />
+                        </a>
+                    </div>
+                    <div className={styles.description2}>
+                        <h2 className={styles.name}>Виктор Иванов</h2>
+                        <h3 className={styles.position}>Програмиране</h3>
+                        <p className={styles.text}>
+                            Казвам се Виктор Иванов, успешно <br /> завърших Американския Колеж в{" "}
+                            <br /> София. Занимавал съм се с <br />
+                            програмиране от 8 клас, където отидох <br /> на областен кръг по
+                            информатика. <br /> От тогава участвам в международния <br /> конгрес
+                            за наука FISSION в категориите <br /> инжинерство и програмиране,
+                            където
+                            <br /> печеля 1 първо и 2 втори места 3-те <br /> години, в които съм
+                            участвал. <br /> Участвах успешно 2 пъти в най-голямото <br />
+                            световно състезание за роботи, FIRST, <br /> където отборът ни за
+                            малко не се <br /> класира в 24-тина финал от 56 отбора.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.member1}>
+                    <div className={styles.description1}>
+                        <h2 className={styles.name}>Красимир Проданов</h2>
+                        <h3 className={styles.position}>Digital Business</h3>
+                        <p className={styles.text}>
+                            Здравейте, казвам се Красимир и съм <br /> на 15 години от София. Уча
+                            в <br /> Софийската Математическа Гимназия <br /> “Паисий
+                            Хилендарски”, като ми <br /> предстои да бъда 10 клас. През 2023
+                            <br /> година спечелих 1 място на ИТ <br /> състезанието в категория{" "}
+                            <br /> “Презентации”, а през настоящата - 3 <br />
+                            място в същата категория. Освен това <br /> много обичам да играя
+                            футбол, да уча <br /> нови езици, като италиански и
+                            <br /> испански, да слушам музика, както и <br /> да свиря на пиано.
+                        </p>
+                    </div>
+                    <div className={styles["person-image"]}>
+                        <a href="https://www.instagram.com/kr.prodanov/" target="_blank">
+                            <img src="./images/krasimir.png" alt="krasimir" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+}
